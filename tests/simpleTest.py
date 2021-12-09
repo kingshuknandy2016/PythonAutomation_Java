ca82e5b8dbf297c91fa55d21b260c887a4e48abd
kingshuk.nandy <kingshuk.nandy@afourtech.com>
2021-12-10 00:21:12 +0530

    Initial Commit

---
+++ tests/simpleTest.py
@@ -0,0 +1,13 @@
+
+# pip install webdriver-manager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+s=Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=s)
+driver.maximize_window()
+driver.get('https://www.javatpoint.com/selenium-python')
+driver.quit()
+
